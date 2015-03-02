62503c25125a116da32d412d1f7050395677167d
Eoin Houlihan <ehoulih@tcd.ie>
2015-03-02 19:37:09 +0000

    CORS on delete methods

--- config/application.rb
+++ config/application.rb
@@ -25,7 +25,7 @@ module HistoryBoard
     config.middleware.insert_before 0, "Rack::Cors" do
       allow do
         origins '*'
-        resource '*', :headers => :any, :methods => [:get, :post, :options]
+        resource '*', :headers => :any, :methods => [:get, :post, :options, :delete]
       end
     end
   end
