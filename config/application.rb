4e43cb7c45855f70cf9a57bf3ca741380003fcbd
Eoin Houlihan <ehoulih@tcd.ie>
2015-03-18 11:58:27 +0000

    Add users and auth

--- config/application.rb
+++ config/application.rb
@@ -25,7 +25,10 @@ module HistoryBoard
     config.middleware.insert_before 0, "Rack::Cors" do
       allow do
         origins '*'
-        resource '*', :headers => :any, :methods => [:get, :post, :options, :delete]
+        resource '*',
+          :headers => :any,
+          :expose  => ['access-token', 'expiry', 'token-type', 'uid', 'client'],
+          :methods => [:get, :post, :options, :delete, :put]
       end
     end
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  mount_devise_token_auth_for 'User', at: 'auth', skip: [:omniauth_callbacks]
   root 'application#root'
   resources :themes, except: [:new, :edit]
   resources :subthemes, except: [:new, :edit]
