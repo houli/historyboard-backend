61135d54e680a9c9f92831854f455b24b6577c68
Peter Meehan <meehanp2@tcd.ie>
2015-03-27 15:56:03 +0000

    added find post by tag function

--- app/models/post.rb
+++ app/models/post.rb
@@ -15,14 +15,17 @@ class Post < ActiveRecord::Base
   mount_uploader :image, ImageUploader
   validates :image, presence: true
 
-  #Getter and Setter for all_tags vertial attribute
   def all_tags=(names)
     self.tags = names.split(",").map do |name|
-        Tag.where(name: name.strip).first_or_create!
+      Tag.where(name: name.strip).first_or_create!
     end
   end
 
   def all_tags
     self.tags.map(&:name)
+  end
+
+  def self.tagged_with(name)
+    Tag.find_by_name!(name).posts
   end
 end
