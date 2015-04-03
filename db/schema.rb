d3ef8a3d70f7eb6d4e48ba7b5ddaa941f7fef25a
Eoin Houlihan <ehoulih@tcd.ie>
2015-04-03 17:51:28 +0100

    Add admin role to Users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150315184241) do
+ActiveRecord::Schema.define(version: 20150327120514) do
 
   create_table "comments", force: :cascade do |t|
     t.text     "body"
@@ -75,6 +75,7 @@ ActiveRecord::Schema.define(version: 20150315184241) do
     t.text     "tokens"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.boolean  "admin"
   end
 
   add_index "users", ["email"], name: "index_users_on_email"
