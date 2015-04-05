413701a43ec850d0f859a4b24cc501de1103218f
Eoin Houlihan <ehoulih@tcd.ie>
2015-04-05 19:47:23 +0100

    Update schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150405182444) do
+ActiveRecord::Schema.define(version: 20150405182635) do
 
   create_table "comments", force: :cascade do |t|
     t.text     "body"
     t.integer  "post_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "user_id"
   end
 
   create_table "posts", force: :cascade do |t|
