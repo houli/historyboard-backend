5c2477cf4152d22cf580e45df94fd0b350a19581
Eoin Houlihan <ehoulih@tcd.ie>
2015-04-07 00:43:35 +0100

    Fix migration error (blame Devise docs)

--- db/schema.rb
+++ db/schema.rb
@@ -56,13 +56,13 @@ ActiveRecord::Schema.define(version: 20150405182635) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string   "provider",                            null: false
-    t.string   "uid",                    default: "", null: false
-    t.string   "encrypted_password",     default: "", null: false
+    t.string   "provider",                               null: false
+    t.string   "uid",                    default: "",    null: false
+    t.string   "encrypted_password",     default: "",    null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,  null: false
+    t.integer  "sign_in_count",          default: 0,     null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
@@ -77,7 +77,7 @@ ActiveRecord::Schema.define(version: 20150405182635) do
     t.text     "tokens"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.boolean  "admin"
+    t.boolean  "admin",                  default: false
   end
 
   add_index "users", ["email"], name: "index_users_on_email"
