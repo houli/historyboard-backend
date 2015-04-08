62f8afb07252b6f5dcd59da7e18944998743fb3e
Eoin Houlihan <ehoulih@tcd.ie>
2015-04-08 12:04:07 +0100

    Don't send full user objects with posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -12,7 +12,7 @@ class PostsController < ApplicationController
       @posts = Post.all
     end
 
-    render json: @posts.to_json(:include => :user)
+    render json: @posts.to_json(include: { user: { only: [:id, :first_name, :last_name] } })
   end
 
   # GET /posts/1
